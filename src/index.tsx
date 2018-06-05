a439fbf656b345ef4ab059c859e3a3c43ce21ce3
Pen <pendleton@muster.com>
2018-06-05 15:39:04 -0400

    add main thread send ports to workers

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,27 @@ import MyWorker from './MyWorker';
 
 const myWorker = new MyWorker();
 
+const mySecondWorker = new MyWorker();
+
+const worker_channel = new MessageChannel();
+
+
+/* first worker */
 myWorker.onmessage = event => {
     // ...
-    console.log('event.data main thread', event.data);
+    console.log('event from first worker', event.data);
 };
 
-myWorker.postMessage('hello worker');
+myWorker.postMessage('hello to first worker from main');
+myWorker.postMessage('sending over the port', [worker_channel.port1]);
+
+/* second worker */
+
+mySecondWorker.onmessage = event => {
+	console.log('evnet from second worker', event.data)
+}
+mySecondWorker.postMessage('hello to second worker from main');
+myWorker.postMessage('sending over the port to worker 2', [worker_channel.port2]);
 
 ReactDOM.render(
   <App />,
